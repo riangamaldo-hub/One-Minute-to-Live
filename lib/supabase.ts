8a735857435034403e77c2898c3ba3bcf70705be
riangamaldo-hub <riangamaldo@gmail.com>
2026-08-30 02:40:23 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 export const SUPABASE_URL = 'https://qmyylopbzozhqljuqfcl.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFteXlsb3Biem96aHFsanVxZmNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgwNTIyMDIsImV4cCI6MjEwMzYyODIwMn0.Wy0Jxe0dyC43WD-qmHad_4xHoukp2kGRKvibTz2739w';
+export const SUPABASE_ANON_KEY = 'sb_publishable_Q1CzzONThfz_NEL4uEXo4w_kMtGYO_L';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
